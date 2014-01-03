1519e116a4e1b6d50c01ea6e05b7c848e7ada0e4
iambilloo <iambilloo@gmail.com>
2014-01-03 11:26:36 +0500

    Matrix Framework ReImagined

---
+++ MatrixFramework/Beem/Matrix/MatrixItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixFramework.Beem.Matrix
+{
+    public abstract class MatrixItem : Object
+    {
+        public abstract override bool Equals(object obj);
+    }
+}
--- MatrixFramework/Program.cs
+++ MatrixFramework/Program.cs
@@ -1,4 +1,5 @@
-﻿using MatrixFramework.Matrix;
+﻿using MatrixFramework.Beem.Matrix;
+using MatrixFramework.Matrix;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,24 +12,69 @@ namespace MatrixFramework
     {
         static void Main(string[] args)
         {
-            Matrix<Me> matrix = new Matrix<Me>(3, 3);
-            matrix.Add(0, 0, new Me() { Name = "B" });
-            matrix.Add(1, 1, new Me() { Name = "I" });
-            matrix.Add(2, 2, new Me() { Name = "L" });
-            for (int i = 0; i < 3; i++)
+            var matrix = new BaseMatrix<Me>(4, 4);
+            char ch = 'a';
+            for (int i = 0; i < 4; i++)
             {
-                for (int j = 0; j < 3; j++)
+                for (int j = 0; j < 4; j++)
+                {
+                    matrix.Add(i, j, new Me() { Name = ch.ToString() });
+                    ch++;
+                }
+            }
+            //matrix.Add(
+            //Matrix<Me> matrix = new Matrix<Me>(3, 3);
+            //matrix.Add(0, 0, new Me() { Name = "B" });
+            //matrix.Add(1, 1, new Me() { Name = "I" });
+            //matrix.Add(2, 2, new Me() { Name = "L" });
+
+            Console.WriteLine("");
+            Console.WriteLine(@"/\/\/\/\/\/\/\/\/\/\");
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
                 {
                     Console.Write(matrix.ElementAt(i, j).Name);
                     Console.Write(" , ");
                 }
                 Console.WriteLine("");
             }
+            Console.WriteLine(@"/\/\/\/\/\/\/\/\/\/\");
+
+            Console.WriteLine("");
+
+            Console.WriteLine("");
+
+            Console.WriteLine(@"/\/\/\/\/\/\/\/\/\/\");
+
+            //matrix.Replace(1, 1, 2, 2);
+            var t = matrix.Swap(1, 1, 2, 2);
+            Console.WriteLine(@"/\/\/\/\/\/\/\/\/\/\");
+
+            Console.WriteLine("");
+
+
+            Console.WriteLine("");
+
+            Console.WriteLine(@"/\/\/\/\/\/\/\/\/\/\");
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(matrix.ElementAt(i, j).Name);
+                    Console.Write(" , ");
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine(@"/\/\/\/\/\/\/\/\/\/\");
+
+            var test = matrix.Find(new Me() { Name = "a" });
+
             Console.ReadKey();
         }
 
     }
-    public class Me
+    public class Me : MatrixItem
     {
         private string name;
 
@@ -45,5 +91,17 @@ namespace MatrixFramework
             set { name = value; }
         }
 
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            else
+            {
+                return this.Name.Equals(((Me)obj).Name);
+            }
+        }
     }
 }
